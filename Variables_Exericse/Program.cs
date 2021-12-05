eb56cdd77b87f5d5b923ff64738d5c3832b9e68e
jdw <johndward01@gmail.com>
2021-12-04 18:59:44 -0600

    first commit

---
+++ Variables_Exericse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Variables_Exericse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            int age = 31;
+            char middleInitial = 'D';
+            bool canVote = true;
+            double score = .99;
+            decimal balance = 10230.98M;
+
+            Console.WriteLine($"Name: {name}\n" +
+                              $"Age: {age}\n" +
+                              $"Middle Initial: {middleInitial}\n" +
+                              $"Can Vote: {canVote}\n" +
+                              $"Score: {score}\n" +
+                              $"Balance: {balance}\n");
+        }
+    }
+}
